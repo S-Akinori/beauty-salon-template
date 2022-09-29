f04dbe611f46ba6db66c5ab049a25f1d09a15ad7
akinori <aki10.working@gmail.com>
2022-09-30 00:33:38 +0900

    first production

--- src/contents/contact.ts
+++ src/contents/contact.ts
@@ -14,14 +14,14 @@ export const contactText = {
   text: "お電話、ホットペッパー、問い合わせフォームよりご予約など受け付けております。お客様のご来店を楽しみにお待ちしてます。",
 }
 
-export const contactLinks: CommonLink[] & {id: string}[] = [
+export const contactLinks: (CommonLink & {id: string})[] = [
   {
     id: 'tel',
     href: 'tel:1234-56-789',
     text: '電話でのご予約、お問い合わせ\n1234-56-789'
   },
   {
-    id: 'tel',
+    id: 'hotpepper',
     href: 'https://beauty.hotpepper.jp/',
     target: '_blank',
     text: 'HOTPEPPER Beautyでのご予約'
